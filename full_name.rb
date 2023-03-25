425218fea07bad301eeb590fe37f3772c29130f5
gacurl <greg.a.curl@gmail.com>
2023-03-25 11:43:02 -0400

    Change prg name

---
+++ full_name.rb
@@ -0,0 +1,2 @@
+# (1) Write a program which asks for a person's first name, then middle, then last.  It should store each of these parts in an array. Finally, it should greet the person using their full name.  Call the program full_name.rb.
+
